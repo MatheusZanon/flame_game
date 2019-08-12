2d6e824ddf4218ecfb0b9fc01f7df03d0638bbac
Matheus <mzblannes@outlook.com>
2019-08-12 11:38:11 -0300

    final da primeira parte

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flame_game/plano_game.dart';
-import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/gestures.dart';
 
 void main() async {
   Util flameUtil = Util();
@@ -9,5 +10,9 @@ void main() async {
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
   PlanoGame game = PlanoGame();
+  TapGestureRecognizer trigger = TapGestureRecognizer();
+  trigger.onTapDown = game.onTapDown;
   runApp(game.widget);
+  flameUtil.addGestureRecognizer(trigger);
+  
 }
